7bc02a3d3925468cca2b189eed74e1eb10daf0fd
Diego Hurtado <diegohurtado@ug.uchile.cl>
2021-08-16 19:56:23 -0400

    fixing repo

--- Tomopoly/Scripts/todo.gd
+++ Tomopoly/Scripts/todo.gd
@@ -1,11 +1,14 @@
+# Esenciales: 
+# TODO Hacer el mecanismo de juego en equipo, quiza agregarle cantidad variable de equipos y que
+#	cada uno los pueda hacer
 # TODO Casos borde por ejemplo cuando no hay jugadores o hay solo 1
 
-# TODO Caso borde de limite de jugadas
 
+# Bonus:
 # TODO Easter egg
 #		Hacer un boton en el menu que salga teclado, y que la pantalla sea solo un teclado y 
 #		un boton enviar, y que con palabras clave se active el juego con pruebas segun la palabra
 #			ejemplo: escribir macelonia pone pruebas de macelonia
 
-# TODO Hacer el mecanismo de juego en equipo, quiza agregarle cantidad variable de equipos y que
-#	cada uno los pueda hacer
+# Detalles, no necesarios:
+# TODO Caso borde de limite de jugadas
